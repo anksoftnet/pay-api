9f129e08180a0a0f0ac5e265edbad98681ea9335
Berke FİNCANCI <berkefin@hotmail.com>
2023-06-30 11:24:20 +0300

    Update callback.php

--- callback.php
+++ callback.php
@@ -1,19 +1,25 @@
 <?php
-$merchantId = '';            // https://www.anksoft.net/odemeapi.php -> Satış Ortaklığı ID'si
-$merchantPassword = '';      // https://www.anksoft.net/odemeapi.php -> Satış Ortaklığı Parolası
+$merchantId = '';           // Satış Ortaklığı ID'si (https://www.anksoft.net/odemeapi.php'den alınabilir)
+$merchantPassword = '';     // Satış Ortaklığı Parolası (https://www.anksoft.net/odemeapi.php'den alınabilir)
 
-if (isset($_POST["status"]) && isset($_POST["order_id"]) && isset($_POST["merchant_id"]) && isset($_POST['hash']) && isset($_POST['amount']) && isset($_POST['full_name'])) { // Callback ile gelen değerleri kontrol ediyoruz. 
+// Gelen POST isteği parametrelerini kontrol ediyoruz
+if (isset($_POST["status"]) && isset($_POST["order_id"]) && isset($_POST["merchant_id"]) && isset($_POST['hash']) && isset($_POST['amount']) && isset($_POST['full_name'])) {
 
-    $hash = base64_encode(hash_hmac('sha256', 'true' .('order_id') . $merchantId, $merchantPassword, true));
+    // Gelen parametrelerle hash değerini oluşturuyoruz
+    $hash = base64_encode(hash_hmac('sha256', 'true' . $_POST['order_id'] . $merchantId, $merchantPassword, true));
 
-    if (post("hash") == $hash) {    // Gelen hash değeri ile oluşturduğumuz hash değerini karşılaştırıyoruz.
-        if ($_POST["status"] == true) {  // Ödeme başarılı ise devam ediyoruz. 
+    // Gelen hash değerini oluşturduğumuz hash ile karşılaştırıyoruz
+    if ($_POST["hash"] == $hash) {
 
-            $paymentData = post("merchant_oid"); // Ödeme Yapan Kullanıcının Kullanıcı Adı
-            $transID = post("order_id"); // Bu değer benzersiz olmalıdır. 
-            $method = "credit_card"; // Ödeme methodu 
-            $credit = post("amount"); // Kullanıcının ödediği tutarı alıyoruz. 
-            $username = post("ExtraInfo"); // Kullanıcı adı veya herhangi bir şey göndermiş olabilirsiniz.
+        // Ödeme başarılıysa devam ediyoruz
+        if ($_POST["status"] == true) {
+
+            // Gerekli ödeme verilerini alıyoruz
+            $paymentData = $_POST["full_name"];    // Ödeme yapan kullanıcının tam adı
+            $transID = $_POST["order_id"];         // Benzersiz sipariş numarası
+            $method = "credit_card";                // Ödeme yöntemi
+            $credit = $_POST["amount"];            // Ödenen miktar
+            $username = $_POST["ExtraInfo"];       // Kullanıcı adı veya diğer ekstra bilgiler
 
             #######################################################################
             #  CALLBACK PARAMETRE ÖRNEKLERİ                                       #
@@ -27,17 +33,19 @@ if (isset($_POST["status"]) && isset($_POST["order_id"]) && isset($_POST["mercha
             # [hash]            ->  string | Kontrol amaçlı şifrelenmiş özel veri #
             #######################################################################
 
-            // Ödeme başarılı ise kredi verme işlemlerini yapabilirsiniz.
-            // Örnek olarak verdiğimiz kodlar ile kredi verme işlemlerini yapabilirsiniz.
-            // Bu kodlar sadece örnek amaçlıdır. Kendi sistemlerinize göre düzenleyebilirsiniz.
+            // Kredi verme işlemlerini burada gerçekleştirebilirsiniz
+            // Örnek olarak verilen kodlar sadece bir örnek olup, kendi sistemlerinize göre düzenlemeniz gerekmektedir
 
-            echo "OK"; // Bu kodu silmeyiniz. Bu kodu silerseniz ödeme işlemi başarısız olarak işaretlenir.
-        } else {  // Ödeme başarısız ise devam ediyoruz. 
+            echo "OK"; // Bu kodu silmeyin, aksi takdirde ödeme işlemi başarısız olarak işaretlenir
+        } else {
+            // Ödeme başarısız ise yine "OK" mesajı gönderiyoruz
             echo "OK";
         }
-    } else {  // Hash değerleri eşleşmiyorsa devam ediyoruz.
+    } else {
+        // Hash değerleri eşleşmiyorsa yine "OK" mesajı gönderiyoruz
         echo "OK";
     }
-} else { // Callback ile gelen değerler eksik ise devam ediyoruz. 
+} else {
+    // Gelen POST isteği parametreleri eksikse yine "OK" mesajı gönderiyoruz
     echo "OK";
 }
